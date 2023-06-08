4d59985df4894f952d5ac9ca17d24b7efcfaf614
Saul Nicolas <Dsaulnicolas@gmail.com>
2023-06-08 17:32:03 -0500

    arracando proyecto de tarjetas. montando clase TarjetaDevito .java

---
+++ java/Tarjrtas/TarjetaDebito.java
@@ -0,0 +1,25 @@
+public class TarjetaDebito {
+	private String cuenta;
+	protected double saldo;
+	public TarjetaDebito (String cunenta, double saldo){
+		this.cuenta = cuenta;
+		this.saldo = saldo;
+	}
+	public double getSaldo () {
+		return saldo;
+	}
+	public String getCuenta (){
+		return cuenta;
+	}
+	public void meterDinero (double monto){
+		saldo +=monto;
+	}
+	public boolean sacarDinero (double monto){
+		if(monto <= saldo){
+			saldo -=monto;
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
