01c4a1578d01e465f33ad902478640902ca1d9eb
Saul Nicolas <Dsaulnicolas@gmail.com>
2023-06-08 17:53:21 -0500

    subiendo prueba de tarjeta de devito .java  los .class

--- java/Tarjrtas/TestTarjetaDevito.java
+++ java/Tarjrtas/TestTarjetaDevito.java
@@ -1,5 +1,5 @@
 public class TestTarjetaDevito {
-	public static void mail (String[] args){
+	public static void main(String[] args){
 		TarjetaDebito tarjeta = new TarjetaDebito("Cuenta 0001", 1000.0);
 		System.out.println("El saldo de la tarjeta es: " + tarjeta.getCuenta() + "\n\tDespues de arrancar con 1000.00 es :" + tarjeta.getSaldo() );
 		tarjeta.meterDinero(500.0);
@@ -7,7 +7,7 @@ public class TestTarjetaDevito {
 		if (tarjeta.sacarDinero(1000.0) == true) {
 			System.out.println("\n\n\tSu saldo despues de retirar 1000 es: " + tarjeta.getSaldo() );
 		}else{
-			System.out.println("\n\n\t No fue posible retirar 1000  );
+			System.out.println("\n\n\t No fue posible retirar 1000" );
 		}
 	}
 }
