4e1d142b0468d7e7774704d45dd11606e2ff0067
Saul <dsaulnicolas@gmail.com>
2023-07-06 16:38:32 -0500

    reparando errores de sintaxis

--- java/CaptarExcepciones/PruebaEntrada.java
+++ java/CaptarExcepciones/PruebaEntrada.java
@@ -2,11 +2,11 @@ import javax.swing.*;
 public class PruebaEntrada {
 	public static void main(String[] args){
 		
-		String entrada = JOptionPlane.showImputDIalog("¿Cuantos años tienes?");
+		String entrada = JOptionPane.showInputDialog("¿Cuantos años tienes?");
 		try {
 			int edad = Integer.parseInt (entrada);
-			System.out.println("Dentro de un año tendra" + (edad + 1) + "años");
-		}catch (NumerFormatException e) {
+			System.out.println("Dentro de un año tendra " + (edad + 1) + " años");
+		}catch (NumberFormatException e) {
 			String cadena = e.getMessage();
 			System.out.println(cadena);
 			System.out.println("error de formato en el numero");
